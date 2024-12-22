ee7dc1c6e96bbda2b2ea2d549ce251ecae2e50ec
Manuel Fabri <manuelnfabri@gmail.com>
2024-12-22 13:55:32 +0100

    summary endpoints

--- src/main/java/com/manuelfabri/expenses/controller/SummaryController.java
+++ src/main/java/com/manuelfabri/expenses/controller/SummaryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +27,20 @@ public class SummaryController {
       @RequestParam(defaultValue = "#{T(java.time.LocalDate).now().getMonthValue()}") int month) {
     return new ResponseEntity<>(this.summaryService.getMonthlySummaryWithTotalBalance(year, month), HttpStatus.OK);
   }
+
+  @GetMapping("/{year}/{month}")
+  public ResponseEntity<List<BalanceSummaryDto>> getSummary(@PathVariable int year, @PathVariable int month) {
+    return new ResponseEntity<>(this.summaryService.getSummary(year, month), HttpStatus.OK);
+  }
+
+  @GetMapping("/{year}")
+  public ResponseEntity<List<BalanceSummaryDto>> getSummary(@PathVariable int year) {
+    return new ResponseEntity<>(this.summaryService.getSummary(year), HttpStatus.OK);
+  }
+
+  @GetMapping("/historic")
+  public ResponseEntity<List<BalanceSummaryDto>> getSummary() {
+    return new ResponseEntity<>(this.summaryService.getSummary(), HttpStatus.OK);
+  }
+
 }
